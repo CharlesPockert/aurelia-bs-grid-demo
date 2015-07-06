a2fdde55f0a90e13e1cf2f02bb6a6124c008ebfb
charlespockert <charles.pockert@googlemail.com>
2015-07-06 16:37:13 +0100

    Update 2015-07-06T15:37:10.757Z

---
+++ jspm_packages/github/gooy/aurelia-compiler@0.0.5.js
@@ -0,0 +1,3 @@
+System.register(["github:gooy/aurelia-compiler@0.0.5/index"], function($__export) {
+  return {  setters: [function(m) { for (var p in m) $__export(p, m[p]); }],  execute: function() {}  };
+});
