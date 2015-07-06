8b77c1d639a71e0354033ff5f80064319cc7e098
charlespockert <charles.pockert@googlemail.com>
2015-07-06 16:33:49 +0100

    Update 2015-07-06T15:33:45.960Z

---
+++ jspm_packages/github/charlespockert/aurelia-bs-grid@master/index.js
@@ -0,0 +1,16 @@
+/* */ 
+System.register([], function (_export) {
+  'use strict';
+
+  _export('configure', configure);
+
+  function configure(aurelia) {
+    aurelia.globalizeResources('./grid/grid');
+    aurelia.globalizeResources('./pager/pager');
+  }
+
+  return {
+    setters: [],
+    execute: function () {}
+  };
+});
